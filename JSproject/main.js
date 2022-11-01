dffc63c5cffb740d699f070650306a4671091b07
nuranajabrail <114071384+nuranajabrail@users.noreply.github.com>
2022-11-01 15:30:26 +0400

    HTML and CSS is done again
    Deleted the repository and created new one. I wrote all HTML and CSS again

---
+++ JSproject/main.js
@@ -0,0 +1,10 @@
+let body = document.body;
+let textarea = document.querySelector('#textarea');
+let add = document.querySelector('#add');
+let buttom = document.querySelector('#circle');
+let todolist = document.querySelector('.todolist');
+let text = document.createElement ('.text');
+let li 
+
+body.append(p);
+
